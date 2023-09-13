5700a8afb5dd1cca14bbb36953adbb877102f802
s.kumar <s.kumar@nadi.ae>
2023-09-13 19:45:14 +0400

    TruckMonitorRepositoryImpl interaction with db and network- code clean up

--- app/src/main/java/com/moovers/monitor/data/repository/TruckMonitorRepositoryImpl.kt
+++ app/src/main/java/com/moovers/monitor/data/repository/TruckMonitorRepositoryImpl.kt
@@ -1,19 +1,28 @@
 package com.moovers.monitor.data.repository
 
 
+import com.moovers.monitor.data.factory.TruckFactory
 import com.moovers.monitor.domain.model.TruckResponseItem
 import com.moovers.monitor.domain.repository.TruckMonitorRepository
+import com.moovers.monitor.util.Source
 import javax.inject.Inject
 
-class TruckMonitorRepositoryImpl
+class TruckMonitorRepositoryImpl @Inject constructor( private val truckFactory: TruckFactory)
     : TruckMonitorRepository {
     override suspend fun fetchTruckDetails(): MutableList<TruckResponseItem> {
-        TODO("Not yet implemented")
+        return truckFactory.create(Source.LOCAL).getTruckDetails()
+            .ifEmpty { cachedTruckDetails() }
     }
 
-    override suspend fun cachedTruckDetails(): MutableList<TruckResponseItem> {
-        TODO("Not yet implemented")
+    override suspend fun cachedTruckDetails() : MutableList<TruckResponseItem>{
+        return truckFactory.create(Source.NETWORK).getTruckDetails()
+            .also { truckDetailsFromNetwork ->
+                truckFactory.create(Source.LOCAL).addTruckDetails(truckDetailsFromNetwork)
+            }
     }
+
+
+
 
 
 }
