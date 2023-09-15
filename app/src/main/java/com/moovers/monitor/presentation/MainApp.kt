7b65d1bf73565c141aa5bb2422c7d0f5fc48b5fb
s.kumar <s.kumar@nadi.ae>
2023-09-16 01:33:12 +0400

    Added the proggress bar

--- app/src/main/java/com/moovers/monitor/presentation/MainApp.kt
+++ app/src/main/java/com/moovers/monitor/presentation/MainApp.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.CircularProgressIndicator
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -29,6 +30,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalContext
@@ -36,6 +38,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
@@ -72,20 +75,21 @@ fun MainScreen(navController: NavHostController, scrollState: ScrollState, viewM
     val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX")
     var truckList by remember { mutableStateOf(mutableListOf<TruckResponseItem>()) }
     var isAscending by remember { mutableStateOf(true) }
+    var progressState by remember { mutableStateOf(true) }
     val textState = remember { mutableStateOf(TextFieldValue("")) }
     val contextForToast = LocalContext.current.applicationContext
 
     when(uiState){
         is TruckViewModel.TruckUiState.Error -> {
-
+            progressState= false
         }
         is TruckViewModel.TruckUiState.Loaded -> {
             val response = (uiState as TruckViewModel.TruckUiState.Loaded).truckResponse
             truckList = response
-
+            progressState= false
         }
         TruckViewModel.TruckUiState.Loading -> {
-
+            progressState= true
         }
 
 
@@ -120,6 +124,7 @@ fun MainScreen(navController: NavHostController, scrollState: ScrollState, viewM
         bottomBar ={
             BottomMenu(navController = navController)
         }) {
+        showProgress(progressState)
         Navigation(
             navController =navController , scrollState =scrollState,
             paddingValues = it,filterList(textState.value.text))
@@ -235,4 +240,23 @@ fun NoTextInSurface(text: String) {
                 )
         }
     }
+}
+@Composable
+fun showProgress(isShown: Boolean) {
+
+    val commentsAlpha = if (isShown) 1f else 0f
+
+
+    Box(
+        modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator(
+            modifier = Modifier
+                .size(40.dp)
+                .alpha(commentsAlpha),
+            color = MaterialTheme.colorScheme.primary,
+            strokeWidth = 2.dp
+        )
+    }
+
 }
